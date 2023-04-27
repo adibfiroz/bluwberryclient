16c7ff82088f47ab515ca07bbbd326fc57584f36
adibfiroz <adibfiroz9702@gmail.com>
2023-04-27 12:38:24 +0530

    changes

--- src/config.js
+++ src/config.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  headers: { Authorization: `Bearer ${access_token}` },
   baseURL: "https://bluwberry.onrender.com/api/",
-  withCredentials: true,
-  crossDomain: true,
 });
 
 export default newRequest;
